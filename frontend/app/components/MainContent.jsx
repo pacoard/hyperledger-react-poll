c3f67dcb4d60a2266b6222e74228eb16746d9ff3
pacoard <pacoard@gmail.com>
2018-02-15 18:24:12 -0600

    Add authentication

--- frontend/app/components/MainContent.jsx
+++ frontend/app/components/MainContent.jsx
@@ -4,6 +4,9 @@ import { selectSideElement } from '../reducers/actions'
 
 import { REST_SERVER } from '../constants/constants'
 
+// To get access token once authenticated
+import cookie from 'react-cookies'
+
 
 class MainContent extends React.Component {
 	constructor(props) {
@@ -57,7 +60,12 @@ class Polls extends React.Component {
 
 	componentDidMount() {
 		// Fetch polls from hyperledger REST API
-		fetch(REST_SERVER + '/api/Poll')
+		var accessToken = cookie.load('access_token');
+		if (!accessToken) {
+			window.location = REST_SERVER + '/auth/github';
+		}
+		console.log(cookie.load('access_token'));
+		fetch(REST_SERVER + '/api/net.biz.poll.Poll', {headers: {'X-Access-Token': accessToken}})
 		.then(result => {
 			return result.json();
 		}).then(data => {
@@ -113,93 +121,92 @@ class NewPoll extends React.Component  {
                 <div className="header">
                     <h4 className="title">New Poll</h4>
                 </div>
-                <div class="content">
+                <div className="content">
                     <form>
-                        <div class="row">
-                            <div class="col-md-5">
-                                <div class="form-group">
+                        <div className="row">
+                            <div className="col-md-5">
+                                <div className="form-group">
                                     <label>Company</label>
-                                    <input type="text" class="form-control border-input" disabled placeholder="Company" value="Creative Code Inc.">
+                                    <input type="text" className="form-control border-input" disabled placeholder="Company" value="Creative Code Inc."/>
                                 </div>
                             </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
+                            <div className="col-md-3">
+                                <div className="form-group">
                                     <label>Username</label>
-                                    <input type="text" class="form-control border-input" placeholder="Username" value="michael23">
+                                    <input type="text" className="form-control border-input" placeholder="Username" value="michael23"/>
                                 </div>
                             </div>
-                            <div class="col-md-4">
-                                <div class="form-group">
+                            <div className="col-md-4">
+                                <div className="form-group">
                                     <label for="exampleInputEmail1">Email address</label>
-                                    <input type="email" class="form-control border-input" placeholder="Email">
+                                    <input type="email" className="form-control border-input" placeholder="Email"/>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="form-group">
                                     <label>First Name</label>
-                                    <input type="text" class="form-control border-input" placeholder="Company" value="Chet">
+                                    <input type="text" className="form-control border-input" placeholder="Company" value="Chet"/>
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
+                            <div className="col-md-6">
+                                <div className="form-group">
                                     <label>Last Name</label>
-                                    <input type="text" class="form-control border-input" placeholder="Last Name" value="Faker">
+                                    <input type="text" className="form-control border-input" placeholder="Last Name" value="Faker"/>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="form-group">
                                     <label>Address</label>
-                                    <input type="text" class="form-control border-input" placeholder="Home Address" value="Melbourne, Australia">
+                                    <input type="text" className="form-control border-input" placeholder="Home Address" value="Melbourne, Australia"/>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="form-group">
+                        <div className="row">
+                            <div className="col-md-4">
+                                <div className="form-group">
                                     <label>City</label>
-                                    <input type="text" class="form-control border-input" placeholder="City" value="Melbourne">
+                                    <input type="text" className="form-control border-input" placeholder="City" value="Melbourne"/>
                                 </div>
                             </div>
-                            <div class="col-md-4">
-                                <div class="form-group">
+                            <div className="col-md-4">
+                                <div className="form-group">
                                     <label>Country</label>
-                                    <input type="text" class="form-control border-input" placeholder="Country" value="Australia">
+                                    <input type="text" className="form-control border-input" placeholder="Country" value="Australia"/>
                                 </div>
                             </div>
-                            <div class="col-md-4">
-                                <div class="form-group">
+                            <div className="col-md-4">
+                                <div className="form-group">
                                     <label>Postal Code</label>
-                                    <input type="number" class="form-control border-input" placeholder="ZIP Code">
+                                    <input type="number" className="form-control border-input" placeholder="ZIP Code"/>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="form-group">
                                     <label>About Me</label>
-                                    <textarea rows="5" class="form-control border-input" placeholder="Here can be your description" value="Mike">Oh so, your weak rhyme
+                                    <textarea rows="5" className="form-control border-input" placeholder="Here can be your description" value="Mike">Oh so, your weak rhyme
 You doubt I'll bother, reading into it
 I'll probably won't, left to my own devices
 But that's the difference in our opinions.</textarea>
                                 </div>
                             </div>
                         </div>
-                        <div class="text-center">
-                            <button type="submit" class="btn btn-info btn-fill btn-wd">Update Profile</button>
+                        <div className="text-center">
+                            <button type="submit" className="btn btn-info btn-fill btn-wd">Update Profile</button>
                         </div>
-                        <div class="clearfix"></div>
+                        <div className="clearfix"></div>
                     </form>
                 </div>
             </div>
-        </div>
 		);
 	}
 }
